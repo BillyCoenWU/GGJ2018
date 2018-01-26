7515373c389e4e8e7e44e7556a3d8f94a76ff022
Rômulo Gomes de Souza Marques dos Santos <romulomarquessantos@hotmail.com>
2018-01-26 20:13:12 -0200

    Commit
    commit

---
+++ Assets/Scripts/Mechanics/HexaTile.cs
@@ -0,0 +1,49 @@
+﻿namespace GGJ
+{
+    using UnityEngine;
+
+    public class HexaTile : MonoBehaviour
+    {
+        public enum TYPE
+        {
+            NORMAL = 0,
+
+            ROCK,
+            TREE,
+            
+            ANIMAL,
+            FOOD
+        }
+
+        public Color color = Colors.White;
+
+        public HexaTile[] aroundTiles = null;
+
+        private int m_indexX = 0;
+        public int indexX
+        {
+            get
+            {
+                return m_indexX;
+            }
+        }
+
+        private int m_indexY = 0;
+        public int indexY
+        {
+            get
+            {
+                return m_indexY;
+            }
+        }
+
+
+        public void SetInfos (int x, int y, Vector2 position, TYPE type)
+        {
+            m_indexX = x;
+            m_indexY = y;
+
+            transform.localPosition = position;
+        }
+    }
+}

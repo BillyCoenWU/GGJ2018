a2605ef00abf643381bb8bb24ae4ee461574aee8
bedulatto <brunobedulatto@hotmail.com>
2018-01-28 11:15:41 -0200

    main menu tutorial framework

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -5,7 +5,7 @@
 
     public class MainMenu : MonoBehaviour
     {
-        public GameObject mainMenu, optionsMenu, title, credits;
+        public GameObject mainMenu, optionsMenu, title, credits, tutorial;
         public Image soundImg;
         public Sprite soundIconOn, soundIconOff;
         bool hasSound;
@@ -22,6 +22,7 @@
             mainMenu.SetActive(true);
             optionsMenu.SetActive(false);
             credits.SetActive(false);
+            tutorial.SetActive(false);
         }
 
         public void Options()
@@ -30,6 +31,7 @@
             mainMenu.SetActive(false);
             optionsMenu.SetActive(true);
             credits.SetActive(false);
+            tutorial.SetActive(false);
         }
 
         public void Credits()
@@ -38,12 +40,22 @@
             mainMenu.SetActive(false);
             optionsMenu.SetActive(false);
             credits.SetActive(true);
+            tutorial.SetActive(false);
         }
 
         public void SoundToggle()
         {
             hasSound = !hasSound;
             soundImg.sprite = hasSound ? soundIconOn : soundIconOff;
+        }
+
+        public void ReadyPlay()
+        {
+            title.SetActive(false);
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(false);
+            credits.SetActive(false);
+            tutorial.SetActive(true);
         }
 
         public void Play()

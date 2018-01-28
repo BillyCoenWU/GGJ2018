e5e0c4cf6453db145162b676b533917207faab13
bedulatto <brunobedulatto@hotmail.com>
2018-01-28 01:35:29 -0200

    Sonar

--- Assets/Scripts/Mechanics/Sonar.cs
+++ Assets/Scripts/Mechanics/Sonar.cs
@@ -7,14 +7,18 @@
         public SpriteRenderer[] sonarSpriteRenderer;
         public Transform[] sonarTransform;
         public Vector3 maxScale;
-        [Range(1,5)]
+        [Range(1, 5)]
         public int maxSonarCount;
         public float delayTime;
+        public float speed;
+        public float alphaSpeed;
 
         public void Init()
         {
+            gameObject.SetActive(true);
             for (int i = 0; i < maxSonarCount; i++)
             {
+                sonarTransform[i].gameObject.SetActive(true);
                 StartCoroutine(EscalonarSonar(i, i * delayTime));
             }
         }
@@ -44,17 +48,24 @@
             yield return new WaitForSeconds(delay);
 
             float lerp = 0.0f;
+            float colorLerp = 0.0f;
 
             while (lerp < 1.0f)
             {
-                lerp += Time.deltaTime;
+                lerp += Time.deltaTime * speed;
+                colorLerp += Time.deltaTime * alphaSpeed;
+                sonarTransform[i].localScale = Vector3.Lerp(Constantes.VECTOR_THREE_ZERO, maxScale, Easings.QuadraticEaseOut(lerp));
 
-                transform.localScale = Vector3.Lerp(Constantes.VECTOR_THREE_ZERO, maxScale, Easings.Linear(lerp));
+                sonarSpriteRenderer[i].color = new Color(1, 1, 1, Mathf.Lerp(1, 0, Easings.BounceEaseInOut(colorLerp)));
 
                 yield return null;
             }
+            sonarTransform[i].gameObject.SetActive(false);
 
-            yield return null;
+            if (i >= maxSonarCount - 1)
+            {
+                Finish();
+            }
         }
 
     }
